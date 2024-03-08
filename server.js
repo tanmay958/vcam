0bc988c3bd5c326cab104a57131c22b443752719
tanmay958 <studytime958@gmail.com>
2024-03-08 23:09:48 +0530

    third

--- server.js
+++ server.js
@@ -26,7 +26,9 @@ app.get("/:room", (req, res) => {
 io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId) => {
     socket.join(roomId);
-    socket.to(roomId).broadcast.emit("user-connected", userId);
+    socket.broadcast.to(roomId).emit("user-connected", userId);
+
+    // socket.to(roomId).broadcast.emit("user-connected", userId);
     // messages
     socket.on("message", (message) => {
       //send message to the same room
